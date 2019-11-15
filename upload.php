9c12777473bf26edaaa6e3bdfaee610b76ca2964
Dominique <dstiekem@student.wethinkcode.co.za>
2019-11-15 11:02:22 +0200

    fixing image stuff

--- upload.php
+++ upload.php
@@ -37,24 +37,6 @@ window.onload = function()
     });
     console.log(canvas);
     var canvasData = canvas.toDataURL("image/png");
-    var x = new XMLHttpRequest();
-    x.open("POST",'save.php',true);
-    x.setRequestHeader('Content-Type', 'application/upload');
-    x.send(canvasData );
-
-    $.ajax({
-    type: "POST",
-    url: "script.php",
-    data: { 
-     imgBase64: dataURL
-    }
-    }).done(function(o) {
-    console.log('saved'); 
-    // If you want the file to be visible in the browser 
-    // - please modify the callback in javascript. All you
-    // need is to return the url to the file, you just saved 
-    // and than put the image in your browser.
-    });
  }
 
 
