16c90d35ee43e24baa382a24c48b701770c6250b
Dom <dom@Doms-iMac.local>
2020-04-14 16:56:16 +0200

    adding 2 other email functions with custom subject lines for forgot password and notification

--- functions/email.php
+++ functions/email.php
@@ -1,7 +1,33 @@
 <?php
-    function sendemail($email, $username, $body)
+    function sendemailnotif($email, $username, $body)
     {
-        $hemail = htmlentities($email, ENT_QUOTES);
+        $hemail = $email;
+        $to = $hemail;
+        $subject = 'Activity On Your Posts';
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $headers[] = 'To:' . $hemail;
+        $headers[] = 'From: Camagru <noreply@camagru.co.za>';
+
+        return (mail($to, $subject, $body, implode("\r\n", $headers)));
+    }
+
+    function sendemailfor($femail, $fusername, $fbody)
+    {
+        $hemail = htmlentities($femail, ENT_QUOTES);
+        $to = $hemail;
+        $subject = 'Reset Password';
+        $headers[] = 'MIME-Version: 1.0';
+        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $headers[] = 'To:' . $hemail;
+        $headers[] = 'From: Camagru <noreply@camagru.co.za>';
+
+        return (mail($to, $subject, $fbody, implode("\r\n", $headers)));
+    }
+
+    function sendemailcon($cemail, $cusername, $cbody)
+    {
+        $hemail = htmlentities($cemail, ENT_QUOTES);
         $to = $hemail;
         $subject = 'Email Confirmation for Camagru Account';
         $headers[] = 'MIME-Version: 1.0';
@@ -10,5 +36,5 @@
         $headers[] = 'From: Camagru <noreply@camagru.co.za>';
 
         return (mail($to, $subject, $body, implode("\r\n", $headers)));
-    }
+    }  
 ?>
